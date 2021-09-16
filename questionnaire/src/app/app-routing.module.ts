bf9f7d5fa731e48cd304dd2daa2a1c2415c69c71
margiebed <margiebed@gmail.com>
2021-09-16 12:12:49 +0200

    add login and styles

--- questionnaire/src/app/app-routing.module.ts
+++ questionnaire/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/services/auth.guard';
+import { LoginComponent } from './core/login/login.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+    // children: [
+    //   { path: "", redirectTo: "employees", pathMatch: "full" },
+    //   { path: "employees", loadChildren: () => import('./questionnaire/questionnaire.module').then(m => m.QuestionnaireModule)},
+    // ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- questionnaire/src/app/app.module.ts
+++ questionnaire/src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material/material.module';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    MaterialModule,
+    BrowserAnimationsModule,
+    AngularFireDatabaseModule,
+    MatPaginatorModule,
+    MatTableModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
