d8ca76476f222f6d09ce9669263260bff2a6f562
margiebed <margiebed@gmail.com>
2021-09-17 12:03:13 +0200

    add login and styles

--- questionnaire/src/app/app.module.ts
+++ questionnaire/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material/material.module';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -23,13 +21,13 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    MaterialModule,
     BrowserAnimationsModule,
-    AngularFireDatabaseModule,
-    MatPaginatorModule,
-    MatTableModule,
-    AngularFireAuthModule
+    SharedModule,
+
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
