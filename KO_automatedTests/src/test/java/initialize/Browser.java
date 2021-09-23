9549b5132c32daea584576cf3e8206272024c415
margiebed <margiebed@gmail.com>
2021-09-23 12:10:30 +0200

    Action automated tests

---
+++ KO_automatedTests/src/test/java/initialize/Browser.java
@@ -0,0 +1,24 @@
+//package initialize;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class Browser {
+//  private static WebDriver driver = new ChromeDriver();
+//
+//  public static WebDriver driver() {
+//    return driver;
+//  }
+//
+//  public static void open(String url) {
+//    driver.get(url);
+//  }
+//
+//  public static void close() {
+//    driver.close();
+//  }
+//
+//  public static void quit() {
+//    driver.quit();
+//  }
+//}
---
+++ KO_automatedTests/src/test/java/pageObject/dashboard/DashboardAssertion.java
@@ -0,0 +1,11 @@
+package pageObject.dashboard;
+
+import org.testng.Assert;
+
+public class DashboardAssertion  {
+  public Dashboard checkThatLoginTextIsDisplyed(Dashboard page){
+
+    Assert.assertEquals(page.getLoginEmailText(), "test@test.com");
+    return new Dashboard();
+  }
+}
